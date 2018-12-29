76050286737ab5b5f04d8aa1184b91e0ce3560f3
王要正 <wangyaozheng@hujiang.com>
2018-12-29 16:59:17 +0800

    Init

---
+++ README.md
@@ -0,0 +1,22 @@
+Convert a file's encoding to what you want.
+
+Usage:
+
+```bash
+file_encoding_convert.py [-h] [-o OUTPUT_FILE] [-d INPUT_ENCODING]
+                                [-e OUTPUT_ENCODING]
+                                input_file
+
+positional arguments:
+  input_file            Input file
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -o OUTPUT_FILE, --output_file OUTPUT_FILE
+                        Output file(this will replace the input file if its
+                        not specified)
+  -d INPUT_ENCODING, --input_encoding INPUT_ENCODING
+                        Input encoding
+  -e OUTPUT_ENCODING, --output_encoding OUTPUT_ENCODING
+                        Output encoding
+```
---
+++ file_encoding_convert.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import sys
+import argparse
+import codecs
+
+def print_error(error_string):
+    sys.stderr.write(error_string + '\n')
+    sys.stderr.flush()
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser(description='Convert a file\'s encoding to what you want.')
+  parser.add_argument('input_file', metavar='input_file', help='Input file', nargs=1)
+  parser.add_argument('-o', '--output_file', help='Output file(this will replace the input file if its not specified)', default=None)
+  parser.add_argument('-d', '--input_encoding', help='Input encoding', default='GBK')
+  parser.add_argument('-e', '--output_encoding', help='Output encoding', default='UTF-8')
+  args = parser.parse_args()
+
+  try:
+    input_file_count = len(args.input_file)
+    for input_file in args.input_file:
+      with codecs.open(input_file, 'rb', encoding=args.input_encoding) as fp:
+        content = fp.read()
+        encoded_content = codecs.encode(content, args.output_encoding)
+        output_file = None if input_file_count > 1 else args.output_file
+        if output_file is None:
+          output_file = input_file
+        with open(output_file, 'wb') as new_fp:
+          new_fp.write(encoded_content)
+  except IOError as error:
+    print_error(error)
+    sys.exit(2)
+  except ValueError as error:
+    print(error)
+    sys.exit(2)
+  else:
+    pass
